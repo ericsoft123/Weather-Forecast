79cb5cf82ed4329551de3e63f8bdb0d9ec11bd35
Eric Kayiranga <ericsoft123@gmail.com>
2021-08-16 05:24:27 +0200

    update setting.py adding .env configuration

--- WeatherProject/settings.py
+++ WeatherProject/settings.py
@@ -1,6 +1,6 @@
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9pwt=y)5$0q^_7d-)z!uspmuig#_hp+6)dion_d!bq@1zhjfdp'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
